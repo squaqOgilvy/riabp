4aee6dfcf3dbea5e5d6331fc3f657a847521c313
Paulo Sanches <paulo.sanch3s@gmail.com>
2013-05-07 14:26:44 -0300

    feature | ogvutil.js | touch | Possui os metodos para bloquear e desbloquar as ações padrão de touchstart, touchmove e touchend.

--- js/lib/ogvutil.js
+++ js/lib/ogvutil.js
@@ -9,9 +9,28 @@
 		// Use for display debug information on screen    	
 		debug: true,
 		// Create a new setTimeout with callback function and duration time params
-        timer: function(callback, duration) {
-            return setTimeout(callback, duration);
-        }
+		timer: function(callback, duration) {
+			return setTimeout(callback, duration);
+		},
+		// Touch Events Handler
+		touch: {
+			// Clear Touch events default action
+			lock: function() {
+				document.addEventListener('touchstart', this.preventDefault, false);
+				document.addEventListener('touchmove', this.preventDefault, false);
+				document.addEventListener('touchend', this.preventDefault, false);
+			},
+			// Restore Touch events to default action
+			unlock: function() {
+				document.removeEventListener('touchstart', this.preventDefault, false);
+				document.removeEventListener('touchmove', this.preventDefault, false);
+				document.removeEventListener('touchend', this.preventDefault, false);
+			},
+			// Prevent default action event
+			preventDefault: function(event) {
+				event.preventDefault();
+			}
+		}
 	};
 
 	window.ogvu = ogvu;
