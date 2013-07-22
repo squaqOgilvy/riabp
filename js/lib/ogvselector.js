df7555d810e2c3139deb4d38bcf296b94fc621db
paulosanches <paulo.sanches@ogilvy.com>
2013-07-22 14:26:42 -0300

    ogvselector add/remove class

--- js/lib/ogvselector.js
+++ js/lib/ogvselector.js
@@ -1,5 +1,5 @@
 /*
-	Author: Ogilvy Brasil	
+	Author: Ogilvy Brasil
 	Developers: Paulo Sanches, Everton Melo
 */
 (function(window) {
@@ -8,8 +8,8 @@
 	var ogvs = function(context) {
 		return new ogvs.fn.init(context);
 	};
-    
-    
+
+
     function selector(sl)
     {
         var elem,
@@ -25,12 +25,12 @@
 					elem = document.getElementsByTagName(sl)[0];
 					break;
 			};
-        
+
         return elem;
     }
-    
+
 	ogvs.fn = ogvs.prototype = {
-		
+
 		init: function(context) {
 			var _c = [],
 			elem,
@@ -39,21 +39,21 @@
 			for(var i=0; i < context.length; i++)
             {
                 if(context.charAt(i) != ' ' ) strAux+= context.charAt(i);
-                
-                if(context.charAt(i) == ' ' || i == context.length-1) 
+
+                if(context.charAt(i) == ' ' || i == context.length-1)
                 {
                     _c.push(strAux);
                     strAux = '';
                 }
             }
-			
+
             if(_c.length>1)
             {
                 for(var j=_c.length-1; j >=0 ; j--)
                 {
                     if(_c[j-1])
                     {
-                        if(!(selector(_c[j]).parentNode == selector(_c[j-1]))) 
+                        if(!(selector(_c[j]).parentNode == selector(_c[j-1])))
                         {
                             console.log("div invalida")
                             return 0;
@@ -61,20 +61,20 @@
                     }
                 }
             }
-            
+
             context = (_c.length > 1)? _c[_c.length-1] : _c[0];
             elem = selector(context);
 
 			if (!elem){console.log('div nula'); return;}
-			
+
             ogvs.fn.methods.call(elem);
             return elem;
-            
+
 		},
-		
-		
+
+
 		methods: function() {
-			// Hide the element with delay 
+			// Hide the element with delay
 			this.hide = function(delay) {
 				if (delay === undefined) {
 					delay = 0;
@@ -84,7 +84,7 @@
 					this.style.display = 'none';
 				}.bind(this), delay * 1000);
 			};
-			// Show the element with delay 
+			// Show the element with delay
 			this.show = function(delay) {
 				if (delay === undefined) {
 					delay = 0;
@@ -142,7 +142,7 @@
 				}.bind(this), delay * 1000);
 			}
 
-			//return string style value 
+			//return string style value
 			this.cssValue = function(property)
 			{
 				return window.getComputedStyle(this, null).getPropertyCSSValue(property).cssText
@@ -156,7 +156,20 @@
 			this.left = function(){return parseInt(this.cssValue('margin-left'));};
 			//return div margin-right
 			this.right = function(){return parseInt(this.cssValue('margin-right'));}
-			
+			//add class
+			this.addClass = function(name, delay) {
+				var $this = this;
+				setTimeout(function() {
+					$this.classList.add(name);
+				}, delay * 1000);
+			};
+			//remove class
+			this.removeClass = function(name, delay) {
+				var $this = this;
+				setTimeout(function() {
+					$this.classList.remove(name);
+				}, delay * 1000);
+			};
 			//set div transformations with cross-browser
 			//@params x, y, rotate = 0, scale = 1
 			this.transform = function(x, y, rotate, scale)
@@ -168,8 +181,8 @@
                this.style.OTransform = "translate("+x+"px, "+y+"px) rotate("+rotate+"deg) scale("+scale+")";
                this.style.transform = "translate("+x+"px, "+y+"px) rotate("+rotate+"deg) scale("+scale+")";
             }
-            
-           
+
+
 		}
 	};
 
